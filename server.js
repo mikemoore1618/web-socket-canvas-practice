ca2da8ecc296390268e81a8f8ee633a81e68120a
Philippe Luchansky <philuchansky@gmail.com>
2018-06-28 23:35:47 -0700

    express server setup

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-// 
+const
+  express = require('express'),
+  app = express(),
+  PORT = process.env.PORT || 3000
+
+app.use(express.static('public'))
+
+app.listen(PORT, (err) => {
+  console.log(err || `Server running on ${PORT}`)
+})
